ca2c593f41ee8d9da9b3921ed882ef4cf227c65b
angelod1as <angelo@angelodias.com.br>
2020-04-23 18:08:49 -0300

    lots of auth stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
 import SignIn from './pages/SignIn/index';
 // import SignUp from './pages/SignUp/index';
 import GlobalStyle from './styles/global';
 
+import { AuthProvider } from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
     <GlobalStyle />
-    <SignIn />
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
   </>
 );
 
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,70 @@
+import React, { createContext, useCallback, useState, useContext } from 'react';
+import api from '../services/api';
+
+interface AuthState {
+  token: string;
+  user: object;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  user: object;
+  signIn(credentials: SignInCredentials): Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@goBarber:token');
+    const user = localStorage.getItem('@goBarber:user');
+
+    if (token && user) {
+      return {
+        token,
+        user: JSON.parse(user),
+      };
+    }
+
+    return {} as AuthState;
+  });
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post<{ token: string; user: object }>(
+      'sessions',
+      {
+        email,
+        password,
+      },
+    );
+
+    const { token, user } = response.data;
+
+    localStorage.setItem('@goBarber:token', token);
+    localStorage.setItem('@goBarber:user', JSON.stringify(user));
+
+    setData({ token, user });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
