7741a911691e065e370d5f51467905d9029ef27d
angelod1as <angelo@angelodias.com.br>
2020-04-22 22:39:14 -0300

    added

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import SignIn from './pages/SignIn/index';
+import SignUp from './pages/SignUp/index';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <SignUp />
+  </>
+);
+
+export default App;
