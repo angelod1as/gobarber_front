19e139234f1cb70eb7b0e2b6cc2083cfcaf7e4e1
angelod1as <angelo@angelodias.com.br>
2020-05-04 20:45:51 -0300

    adding

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import SignIn from './pages/SignIn/index';
-// import SignUp from './pages/SignUp/index';
 import GlobalStyle from './styles/global';
+
+import Routes from './routes';
 
 import AppProvider from './hooks';
 
 const App: React.FC = () => (
-  <>
+  <Router>
     <AppProvider>
-      <SignIn />
+      <Routes />
     </AppProvider>
 
     <GlobalStyle />
-  </>
+  </Router>
 );
 
 export default App;
