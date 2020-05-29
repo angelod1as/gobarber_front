8f8530787c43c3172edc27d8df2ac5b4dfca4a26
angelod1as <angelo@angelodias.com.br>
2020-05-29 15:48:16 -0300

    finishing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,6 +9,7 @@ import ForgotPassword from '../pages/ForgotPassword';
 import ResetPassword from '../pages/ResetPassword';
 
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 const Routes: React.FC = () => {
   return (
@@ -18,6 +19,7 @@ const Routes: React.FC = () => {
       <Route path="/forgot-password" component={ForgotPassword} />
       <Route path="/reset-password" component={ResetPassword} />
 
+      <Route path="/profile" component={Profile} isPrivate />
       <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
